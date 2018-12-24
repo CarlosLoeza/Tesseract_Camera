334a262491ea8c7ab5644535ae474ac10b5e2b16
Carlos Loeza <c.loeza1116@gmail.com>
2018-12-24 00:52:38 -0800

    Imported Tesseract OCR * I created a pod file to implement my tesseractOCR for my text recognition project * I used imported multiple images in my assets folder to test out the test recognition

--- Project Camouflage/ViewController.swift
+++ Project Camouflage/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import TesseractOCR
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, G8TesseractDelegate {
+    
+    let tesseract: G8Tesseract = G8Tesseract(language: "eng")!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Running OCR")
+        
+        tesseract.delegate = self
+        tesseract.charWhitelist = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        
+        // Select the image we would like to scan using tesseract
+        let imageToCheck = UIImage(named: "welcomePhoto")
+        // Runs the text recognition on the image given above
+        tesseract.image = imageToCheck!
+        tesseract.recognize()
+        // Print the text from the given image
+        print("The text is \(tesseract.recognizedText!)")
+    
+    }
+    
+    
+    // Function will cancel image recognition if the switches to another screen or exits out of the app
+    // We don't want the OCR trying to recognize objects once user has exited
+    func shouldCancelImageRecognition(for tesseract: G8Tesseract) -> Bool {
+        return false
     }
 
 
